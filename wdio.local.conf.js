5d8c034d18ef482266a4c9ac572c133b2c98519e
Narahari Miriyala <miriyalanarahari@gmail.com>
2025-09-16 10:04:41 +0100

    update base url

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -112,7 +112,7 @@ export const config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: 'https://grants-ui.test.cdp-int.defra.cloud',
+  baseUrl: 'http://localhost:3000',
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
