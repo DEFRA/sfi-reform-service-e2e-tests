f436c06806dacd96ba41d5e317454cc61cd71951
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2025-10-30 16:30:45 +0000

    updated

--- test/specs/farmer_e2e_journey.js
+++ test/specs/farmer_e2e_journey.js
@@ -10,6 +10,9 @@ import CwAllCasesPage from '../page-objects/cw.allcases.page.js'
 import CwApplicationPage from '../page-objects/cw.application.page.js'
 import CwTimelinePage from '../page-objects/cw.timeline.page.js'
 import CWAgreementsPage from '../page-objects/cw.agreements.page.js'
+import AgreementReviewOfferPage from '../page-objects/agreements.review.offer.page.js'
+import AgreementsAcceptYourOfferPage from '../page-objects/agreements.accept.your.offer.page.js'
+import AgreementOfferAcceptedPage from '../page-objects/agreements.offer.accepted.page.js'
 
 afterEach(async () => {
   // Clear all cookies after each test
@@ -31,6 +34,7 @@ describe('SFI Application E2E Tests', () => {
       await HomePage.open()
       await browser.pause(5000)
       console.log(`Application Reference Number:`, appRefNum)
+      // CW Approval Process
       await browser.url(browser.options.cwUrl)
       const cwUsername = process.env.ENTRA_ID_ADMIN_USER
       const cwPassword = process.env.ENTRA_ID_USER_PASSWORD
@@ -73,26 +77,27 @@ describe('SFI Application E2E Tests', () => {
       await browser.url(
         browser.options.agreementsUrl + agreementIdInitialJourney
       )
-      // await browser.pause(5000)
-      // await AgreementReviewOfferPage.selectContinue()
-      // await AgreementsAcceptYourOfferPage.selectAcceptOffer()
-      // const confirmationText =
-      //   await AgreementOfferAcceptedPage.getConfirmationText()
-      // expect(confirmationText).toBe('Offer accepted')
-      //
-      // // Case Working - Verify Agreement Status after Farmer Accepts Offer
-      // await browser.url(browser.options.cwUrl)
-      // await CWHomePage.clickLinkByText(appRefNum)
-      // await browser.pause(5000)
-      // await CwTimelinePage.clickLinkByText('Agreements')
-      // await browser.pause(5000)
-      // const agreementIdOnReturn =
-      //   await CWAgreementsPage.getFirstAgreementReferenceText()
-      // expect(agreementIdInitialJourney).toBe(agreementIdOnReturn)
-      // expect(await CWAgreementsPage.getFirstAgreementStatusText()).toBe(
-      //   'Accepted'
-      // )
-      // await browser.pause(5000)
+      await browser.pause(5000)
+      await AgreementReviewOfferPage.selectContinue()
+      await AgreementsAcceptYourOfferPage.selectAcceptOffer()
+      const confirmationText =
+        await AgreementOfferAcceptedPage.getConfirmationText()
+      expect(confirmationText).toBe('Offer accepted')
+
+      // Case Working - Verify Agreement Status after Farmer Accepts Offer
+      await browser.pause(5000)
+      await browser.url(browser.options.cwUrl)
+      await CWHomePage.clickLinkByText(appRefNum)
+      await browser.pause(5000)
+      await CwTimelinePage.clickLinkByText('Agreements')
+      await browser.pause(5000)
+      const agreementIdOnReturn =
+        await CWAgreementsPage.getFirstAgreementReferenceText()
+      expect(agreementIdInitialJourney).toBe(agreementIdOnReturn)
+      expect(await CWAgreementsPage.getFirstAgreementStatusText()).toBe(
+        'Accepted'
+      )
+      await browser.pause(5000)
     })
   })
 })
