6905039a0cdb29e8e4e569a2d991530450362dc7
Mayank Solanki <manky18@gmail.com>
2026-04-29 17:19:34 +0100

    refactor the wmp journey flow

--- test/page-objects/woodland.home.page.js
+++ test/page-objects/woodland.home.page.js
@@ -1,3 +1,4 @@
+import { browser, $ } from '@wdio/globals'
 import { Page } from '../page-objects/page'
 
 class WoodlandHomePage extends Page {
@@ -7,6 +8,146 @@ class WoodlandHomePage extends Page {
 
   clearApplicationState() {
     return super.open('/woodland/clear-application-state')
+  }
+
+  async completeCheckDetails() {
+    await this.selectRadioById('businessDetailsUpToDate')
+    await this.submit()
+  }
+
+  async completeEligibility() {
+    await this.clickHref('/woodland/eligibility-land-registered')
+    await this.waitForUrlIncludes('/woodland/eligibility-land-registered')
+    await this.selectRadioById('landRegisteredWithRpa')
+    await this.submit()
+
+    await this.selectRadioById('landManagementControl')
+    await this.submit()
+
+    await this.selectRadioById('publicBodyTenant-2')
+    await this.submit()
+
+    await this.selectRadioById('landHasGrazingRights-2')
+    await this.submit()
+
+    await this.selectRadioById('appLandHasExistingWmp-2')
+    await this.submit()
+
+    await this.selectRadioById('intendToApplyHigherTier')
+    await this.submit()
+  }
+
+  async completeWoodlandDetails({
+    landParcelId,
+    hectaresTenOrOverYearsOld,
+    centreGridReference,
+    woodlandName,
+    fcTeamCodeId
+  }) {
+    await this.clickHref('/woodland/land-parcels')
+    await this.waitForUrlIncludes('/woodland/land-parcels')
+    await this.selectCheckboxByValue(landParcelId)
+    await this.submit()
+
+    await this.typeById(
+      'hectaresTenOrOverYearsOld',
+      String(hectaresTenOrOverYearsOld)
+    )
+    await this.submit()
+
+    await this.typeById('centreGridReference', centreGridReference)
+    await this.submit()
+
+    await this.typeById('woodlandName', woodlandName)
+    await this.submit()
+
+    await this.selectRadioById(fcTeamCodeId)
+    await this.submit()
+  }
+
+  async submitApplication() {
+    await this.clickHref('/woodland/summary')
+    await this.waitForUrlIncludes('/woodland/summary')
+    await this.submit()
+    await this.submit()
+    await this.submit()
+  }
+
+  async getApplicationReference() {
+    const refNumberEl = await $('.govuk-panel__body strong')
+    await refNumberEl.waitForDisplayed()
+    return (await refNumberEl.getText()).toLowerCase()
+  }
+
+  async submit() {
+    const submitButton = await $('button[type="submit"]')
+    await submitButton.waitForClickable()
+    await submitButton.click()
+  }
+
+  async selectRadioById(id) {
+    const label = await $(`label[for="${id}"]`)
+    if (await label.isExisting()) {
+      await label.waitForClickable()
+      await label.click()
+      return
+    }
+
+    const radio = await $(`#${id}`)
+    await radio.waitForExist()
+    await browser.execute((el) => el.click(), radio)
+  }
+
+  async selectCheckboxByValue(value) {
+    const safeValue = String(value).replace(/"/g, '\\"')
+    const checkbox = await $(`input[type="checkbox"][value="${safeValue}"]`)
+
+    await checkbox.waitForExist({
+      timeout: 50000,
+      timeoutMsg: `Checkbox with value "${value}" was not found`
+    })
+
+    if (await checkbox.isSelected()) {
+      return
+    }
+
+    const checkboxId = await checkbox.getAttribute('id')
+    if (checkboxId) {
+      const label = await $(`label[for="${checkboxId}"]`)
+      if (await label.isExisting()) {
+        await label.waitForClickable()
+        await label.click()
+        return
+      }
+    }
+
+    await browser.execute((el) => el.click(), checkbox)
+  }
+
+  async typeById(id, value) {
+    const input = await $(`#${id}`)
+    await input.waitForDisplayed()
+    await input.clearValue()
+    await input.setValue(value)
+  }
+
+  async waitForUrlIncludes(path, timeout = 50000) {
+    await browser.waitUntil(
+      async () => (await browser.getUrl()).includes(path),
+      {
+        timeout,
+        timeoutMsg: `Expected URL to include "${path}" but it did not`
+      }
+    )
+  }
+
+  async clickHref(path) {
+    const link = await $(`a[href="${path}"]`)
+    await link.waitForClickable({
+      timeout: 50000,
+      timeoutMsg: `Link with href "${path}" was not clickable`
+    })
+    await link.click()
   }
 }
 
--- test/specs/woodland_management_journey.js
+++ test/specs/woodland_management_journey.js
@@ -1,9 +1,6 @@
 import { browser } from '@wdio/globals'
 import { completeWoodlandJourney } from '../utils/cw-journey-helper.js'
-import {
-  clearWoodlandState,
-  loginAndRunWoodlandManagementJourney
-} from '../utils/woodland-journey-helper.js'
+import { loginAndRunWoodlandManagementJourney } from '../utils/woodland-journey-helper.js'
 
 afterEach(async () => {
   // Clear all cookies after each test
@@ -12,16 +9,26 @@ afterEach(async () => {
 
 describe('Woodland Management Plan E2E Tests', () => {
   it('The farmer is able to complete the Woodland Management Plan application', async () => {
-    const username = '1106298365'
-    const password = process.env.DEFRA_ID_USER_PASSWORD
+    const testUser = {
+      username: '1106298365',
+      password: process.env.DEFRA_ID_USER_PASSWORD
+    }
 
-    // Clear any in-progress woodland application state
-    await clearWoodlandState(username, password)
+    const woodlandApplicationData = {
+      landParcelId: 'NT8701-9412',
+      hectaresTenOrOverYearsOld: 50,
+      centreGridReference: 'SP 4178 2432',
+      woodlandName: 'Ashbrook WD',
+      fcTeamCodeId: 'fcTeamCode-2'
+    }
+
+    const { username, password } = testUser
 
     // Complete the woodland application journey via the UI
     const { appRefNum } = await loginAndRunWoodlandManagementJourney({
       username,
-      password
+      password,
+      applicationData: woodlandApplicationData
     })
 
     // CW Approval Process
